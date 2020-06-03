dd9a2c4b53ca473fd617ad79dc1fb55dd3e2e349
Nabieu Ishmeal Bangura <nabieuishmealbangura@nab.local>
2020-06-04 03:16:51 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import string
+import random
+
+
+def generatePassword(num):
+    password = ''
+
+    for n in range(num):
+        x = random.randint(0, 94)
+        password += string.printable[x]
+
+    return password
+
+
+# generate and print a 16 character random password
+
+print(generatePassword(16))
